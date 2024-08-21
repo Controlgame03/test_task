422bf5f026f5b2d7a545de15fae7e5e9c04f5bbd
Denis Sergeev <denserg765@gmail.com>
2024-08-21 11:33:57 +0300

    edited getPackages method

--- src/com/example/com/example/RdbApiClient.java
+++ src/com/example/com/example/RdbApiClient.java
@@ -3,6 +3,8 @@ package com.example;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,26 +15,43 @@ public class RdbApiClient {
     private static final String API_URL = "https://rdb.altlinux.org/api/export/branch_binary_packages/";
 
     public List<PackageInfo> getPackagesForBranch(String branch) throws Exception {
-        String urlStr = API_URL + branch;
-        URL url = new URL(urlStr);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
+    	// Формируем URL
+        String urlStr = API_URL + branch + "?arch=" + "x86_64";;
+        try {
+        	 URI uri = new URI(urlStr);
+             URL url = uri.toURL();
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        
-        
-        content.append(in.readLine());
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
+             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+         
+             conn.setRequestMethod("GET");
+             
+             
+             
+             
+             
+             // Обработка ответа
+             if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                 throw new RuntimeException("Failed to get response from API: HTTP code " + conn.getResponseCode());
+             }
+
+             BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+             String inputLine;
+             
+             
+             inputLine = in.readLine();
+//             inputLine = in.readLine();
+//             inputLine = in.readLine();
+//             while ((inputLine = in.readLine()) != null) {
+//                 content.append(inputLine);
+//             }
+             StringBuilder content = new StringBuilder(inputLine);
+             in.close();
+             conn.disconnect();
+
+             return parsePackages(content.toString());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Invalid URL: " + urlStr, e);
         }
-        
-
-        in.close();
-        conn.disconnect();
-
-        return parsePackages(content.toString());
     }
 
     private List<PackageInfo> parsePackages(String jsonStr) {
